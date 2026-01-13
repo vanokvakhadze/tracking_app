6529fb7e8feede8ce19f6f8a15dfb4016e953dd0
vanokvakhadze <vkvakhadze35@gmail.com>
2026-01-14 03:48:47 +0400

    Initial Commit

---
+++ Wtracking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wtracking
+//
+//  Created by vano Kvakhadze on 14.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wtracking/WtrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WtrackingApp.swift
+//  Wtracking
+//
+//  Created by vano Kvakhadze on 14.01.26.
+//
+
+import SwiftUI
+
+@main
+struct WtrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
